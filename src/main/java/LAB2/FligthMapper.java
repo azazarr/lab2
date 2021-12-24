d9892261df236cf7e635d4afd3155f70844e159d
niki <nikitapisarenko1995@gmail.com>
2021-12-24 20:49:41 +0300

    Scripted auto-commit on change (2021-12-24 20:49:41) by gitwatch.sh

--- src/main/java/LAB2/FligthMapper.java
+++ src/main/java/LAB2/FligthMapper.java
@@ -5,6 +5,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.LongWritable;
+import java.io.IOException;
 
 public class FligthMapper {
+    
 }
