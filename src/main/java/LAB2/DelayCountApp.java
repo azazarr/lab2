bee51c48957d7b2b9f227864638e36b0246dcf0d
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:06:02 +0300

    Scripted auto-commit on change (2021-12-24 21:06:02) by gitwatch.sh

--- src/main/java/LAB2/DelayCountApp.java
+++ src/main/java/LAB2/DelayCountApp.java
@@ -21,7 +21,7 @@ public class WordCountApp {
         job.setJarByClass(WordCountApp.class);
         job.setJobName("Delay count");
         MultipleInputs.addInputPath(job, new Path(args[0]));
-        MultipleInputs.setOutputPath(job, new Path(args[1]));
+        MultipleInputs.addInputPath(job, new Path(args[0]));
         job.setMapperClass(WordMapper.class);
         job.setReducerClass(WordReducer.class);
         job.setOutputKeyClass(Text.class);
