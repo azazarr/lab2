78499642e390c16987fc377f02f3077870d8f28c
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:20:38 +0300

    Scripted auto-commit on change (2021-12-24 21:20:38) by gitwatch.sh

--- src/main/java/LAB2/GroupCompator.java
+++ src/main/java/LAB2/GroupCompator.java
@@ -7,6 +7,6 @@ import org.apache.hadoop.mapreduce.v2.app.webapp.dao.CounterGroupInfo;
 
 public class GroupCompator extends WritableComparable {
     protected GroupCompator () {
-        super()
+        super(Airpo)
     }
 }
