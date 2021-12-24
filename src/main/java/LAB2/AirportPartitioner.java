5160dec5ffe75c47b536d823f9a25ba1225835d1
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:18:21 +0300

    Scripted auto-commit on change (2021-12-24 21:18:21) by gitwatch.sh

--- src/main/java/LAB2/AirportPartitioner.java
+++ src/main/java/LAB2/AirportPartitioner.java
@@ -6,9 +6,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.LongWritable;
 
-public class AirportPartitioner extends Partitioner<AirportWritable{
-
+public class AirportPartitioner extends Partitioner <AirportWritable,Text> {
+    @Override
     public int getPartition(AirportWritable key, Text value, int nReduceTask){
-        return key.getAIRID() % nReduceTask
+        return key.getAIRID() % nReduceTask;
     }
 }
