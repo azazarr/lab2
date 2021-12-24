599f215eb58513b3609638d179609bd2350ee188
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:04:22 +0300

    Scripted auto-commit on change (2021-12-24 21:04:22) by gitwatch.sh

--- src/main/java/LAB2/DelayCountApp.java
+++ src/main/java/LAB2/DelayCountApp.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.FileOut;
+import org.apache.hadoop.mapreduce.lib.input.FileOutputFormat;
 import org.apache.hadoop.io.LongWritable;
 
 import java.io.IOException;
