1180f818611ca95dbc6206d36318e45afd6fe7d5
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:19:23 +0300

    Scripted auto-commit on change (2021-12-24 21:19:23) by gitwatch.sh

--- src/main/java/LAB2/GroupCompator.java
+++ src/main/java/LAB2/GroupCompator.java
@@ -1,8 +1,9 @@
 package LAB2;
 
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
 
-public class GroupCompator extends  {
+public class GroupCompator extends WritableComparable {
 
 }
