59a7d28313f7abc6fd4d0dbe9b56adb305bcea11
niki <nikitapisarenko1995@gmail.com>
2021-12-24 22:03:30 +0300

    Scripted auto-commit on change (2021-12-24 22:03:30) by gitwatch.sh

--- src/main/java/LAB2/ReducerJoinReducer.java
+++ src/main/java/LAB2/ReducerJoinReducer.java
@@ -3,9 +3,9 @@ package LAB2;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-
-import java.io.IOException;
 import java.util.Iterator;
+import java.io.IOException;
+
 
 
 public class ReducerJoinReducer extends  Reducer<AirportWritable,Text, IntWritable, Text> {
