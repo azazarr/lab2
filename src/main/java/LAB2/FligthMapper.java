9cc711724fdf7a4b0491b187d922c086363ef7da
niki <nikitapisarenko1995@gmail.com>
2021-12-25 00:12:16 +0300

    Scripted auto-commit on change (2021-12-25 00:12:16) by gitwatch.sh

--- src/main/java/LAB2/FligthMapper.java
+++ src/main/java/LAB2/FligthMapper.java
@@ -7,8 +7,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FligthMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
-    private static final int ID = 0;
-    private static final int DEL = 0;
+    private static final int ID = 14;
+    private static final int DEL = ;
     private static final int FID = 0;
 
     protected void map (LongWritable key, Text value, Context context) throws IOException, InterruptedException {
