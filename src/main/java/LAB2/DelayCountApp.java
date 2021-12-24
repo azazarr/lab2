7f536bbeb32d75cecd996d45169f63425b7e3665
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:14:44 +0300

    Scripted auto-commit on change (2021-12-24 21:14:44) by gitwatch.sh

--- src/main/java/LAB2/DelayCountApp.java
+++ src/main/java/LAB2/DelayCountApp.java
@@ -26,7 +26,7 @@ public class WordCountApp {
         job.setPartitionerClass(AirportPartitioner.class);
         job.setGroupingComparatorClass(GroupCompator.class);
         job.setReducerClass(ReducerJoinReducer.class);
-        job.setMapOutputKeyClass();
+        job.setMapOutputKeyClass(Ai);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
