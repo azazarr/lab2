50c9fe3a2f98fc3bcd118fae665bb5601753e1dd
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:21:50 +0300

    Scripted auto-commit on change (2021-12-24 21:21:50) by gitwatch.sh

--- src/main/java/LAB2/GroupCompator.java
+++ src/main/java/LAB2/GroupCompator.java
@@ -12,6 +12,6 @@ public class GroupCompator extends WritableComparable {
     }
 
     public int compare(WritableComparable w1, WritableComparable w2) {
-        return ((AirportWritable))
+        return ((AirportWritable)w1)
     }
 }
