7f50284fb04069e7b2fde198a07654ea3566a065
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:55:56 +0300

    Scripted auto-commit on change (2021-12-24 21:55:56) by gitwatch.sh

--- src/main/java/LAB2/ReducerJoinReducer.java
+++ src/main/java/LAB2/ReducerJoinReducer.java
@@ -26,6 +26,6 @@ public class ReducerJoinReducer extends  Reducer<AirportWritable,Text, IntWritab
             min = Math.min(delay, min);
         }
         String resV = String.format("%s %.2f %.2f %.2f %.2f", airportName, totalDelay/country,max,min,totalDelay);
-        
+        if (counter >= 0)
     }
 }
