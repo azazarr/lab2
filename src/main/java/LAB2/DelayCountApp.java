1aa2f974a932a9b00f7d7d1d98481cb649c2fb20
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:01:17 +0300

    Scripted auto-commit on change (2021-12-24 21:01:17) by gitwatch.sh

--- src/main/java/LAB2/DelayCountApp.java
+++ src/main/java/LAB2/DelayCountApp.java
@@ -1,3 +1,8 @@
 package LAB2;
 
-private static final int ID = 0;
+public class WordCountApp {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.exit(-1);
+        }
