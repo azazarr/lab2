baa5b461d0c3aa4975dae5b752f23ca092150ab0
niki <nikitapisarenko1995@gmail.com>
2021-12-24 20:35:39 +0300

    Scripted auto-commit on change (2021-12-24 20:35:39) by gitwatch.sh

--- src/main/java/LAB2/AirportMapper.java
+++ src/main/java/LAB2/AirportMapper.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
     private static final int helper = 0;
     private static final int AIR_ID = 0;
+    
     protected void map (LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
                 String[] airposrts = value.toString().split("");
