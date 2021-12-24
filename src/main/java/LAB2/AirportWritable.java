d5555c7fc36759fb6cb1d2bb9ca20c3e7aa4e9c2
niki <nikitapisarenko1995@gmail.com>
2021-12-24 20:20:11 +0300

    Scripted auto-commit on change (2021-12-24 20:20:11) by gitwatch.sh

--- src/main/java/LAB2/AirportWritable.java
+++ src/main/java/LAB2/AirportWritable.java
@@ -10,7 +10,7 @@ import org.apache.hadoop.io.LongWritable;
 public class AirportWritable {
     int AIRID;
 
-    puvlic Air
+    puvlic Airport
 
     public int getAIRID() {
         return AIRID;
