fed4faf1f3e5d5e350c6e50a7ad3a353903a0685
niki <nikitapisarenko1995@gmail.com>
2021-12-24 22:12:35 +0300

    Scripted auto-commit on change (2021-12-24 22:12:35) by gitwatch.sh

--- src/main/java/LAB2/AirportWritable.java
+++ src/main/java/LAB2/AirportWritable.java
@@ -7,7 +7,7 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class AirportWritable implements WritableComparable {
+public class AirportWritable implements WritableComparable<Airt> {
     int AIRID, IND;
 
     public AirportWritable () {}
