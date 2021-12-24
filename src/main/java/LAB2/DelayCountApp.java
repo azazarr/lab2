d6206c2e5c2f03d1e17eaf2baa94b6ae188df2d9
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:01:54 +0300

    Scripted auto-commit on change (2021-12-24 21:01:54) by gitwatch.sh

--- src/main/java/LAB2/DelayCountApp.java
+++ src/main/java/LAB2/DelayCountApp.java
@@ -1,5 +1,13 @@
 package LAB2;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.io.LongWritable;
+
+import java.io.IOException;
+
 public class WordCountApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
