92ab1ee387ef89f0f617c3c7b268136343d78580
niki <nikitapisarenko1995@gmail.com>
2021-12-24 19:42:55 +0300

    Scripted auto-commit on change (2021-12-24 19:42:55) by gitwatch.sh

--- src/main/java/LAB2/AirportMapper.java
+++ src/main/java/LAB2/AirportMapper.java
@@ -5,7 +5,7 @@ package LAB2;
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
     private static final int helper = 0;
     private static final int AIR_ID = 0;
-    
+    protected void map 
 
 
 }
