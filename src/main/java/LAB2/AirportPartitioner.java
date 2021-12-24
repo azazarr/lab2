0f354b7cefb63f0f1fe2387ce9d028f65791b48e
niki <nikitapisarenko1995@gmail.com>
2021-12-24 19:48:04 +0300

    Scripted auto-commit on change (2021-12-24 19:48:04) by gitwatch.sh

--- src/main/java/LAB2/AirportPartitioner.java
+++ src/main/java/LAB2/AirportPartitioner.java
@@ -9,6 +9,6 @@ import org.apache.hadoop.io.LongWritable;
 public class AirportPartitioner {
     @Override
     public int getPartition(AirportWritable key, Text value, int nReduceTask){
-        reduce 
+        return 
     }
 }
