88185c3715d0e6dcdc6ee9e529e104c18721c003
niki <nikitapisarenko1995@gmail.com>
2021-12-24 20:16:09 +0300

    Scripted auto-commit on change (2021-12-24 20:16:09) by gitwatch.sh

--- src/main/java/LAB2/AirportWritable.java
+++ src/main/java/LAB2/AirportWritable.java
@@ -2,7 +2,7 @@ package LAB2;
 
 import org.apache.hadoop.io.WritableComparable;
 import java.io.DataInput;
-import java.io.DataInput;
+import java.io.DataOutput;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.LongWritable;
