3e81c65458c666ab16ac3eaae34ed26a4c10e0b8
niki <nikitapisarenko1995@gmail.com>
2021-12-24 22:21:50 +0300

    Scripted auto-commit on change (2021-12-24 22:21:50) by gitwatch.sh

--- src/main/java/LAB2/AirportWritable.java
+++ src/main/java/LAB2/AirportWritable.java
@@ -31,7 +31,7 @@ public class AirportWritable implements WritableComparable<AirportWritable> {
     @Override
     public void write(DataOutput dataOutput) throws IOException {
         AIRID = dataOutput.writeInt;
-        IND = dataOutput.readInt();
+        IND = dataOutput.writeInt();
     }
 
     @Override
