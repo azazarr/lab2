78657fdab3046119abacddbb00ac9497eb722b6d
niki <nikitapisarenko1995@gmail.com>
2021-12-24 23:26:24 +0300

    Scripted auto-commit on change (2021-12-24 23:26:24) by gitwatch.sh

--- src/main/java/LAB2/WordCountApp.java
+++ src/main/java/LAB2/WordCountApp.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCountApp {
     public static void main(String[] args) throws Exception {
@@ -18,7 +19,7 @@ public class WordCountApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FligthMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
 
-        F
+        FileOutputFormat
         job.setPartitionerClass(AirportPartitioner.class);
         job.setGroupingComparatorClass(GroupCompator.class);
         job.setReducerClass(ReducerJoinReducer.class);
