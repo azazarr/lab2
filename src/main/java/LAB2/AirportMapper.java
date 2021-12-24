ea6d3ec66038d97325d6b34b45bb0d646efba5ef
niki <nikitapisarenko1995@gmail.com>
2021-12-24 19:49:26 +0300

    Scripted auto-commit on change (2021-12-24 19:49:26) by gitwatch.sh

--- src/main/java/LAB2/AirportMapper.java
+++ src/main/java/LAB2/AirportMapper.java
@@ -6,11 +6,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.LongWritable;
 
+import java.io.IOException;
+
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
     private static final int helper = 0;
     private static final int AIR_ID = 0;
-    protected void map (LongWritable key, Text value, Context context) throws IO
+    protected void map (LongWritable key, Text value, Context context) throws IOException
 
 
 }
