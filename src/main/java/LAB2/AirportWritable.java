4c415e79932c0c6345f8fabd6c1ef542a25f23a6
niki <nikitapisarenko1995@gmail.com>
2021-12-24 20:43:57 +0300

    Scripted auto-commit on change (2021-12-24 20:43:57) by gitwatch.sh

--- src/main/java/LAB2/AirportWritable.java
+++ src/main/java/LAB2/AirportWritable.java
@@ -41,8 +41,7 @@ public class AirportWritable {
     public int CompareTo(AirportWritable o) {
         if (AIRID == o.AIRID)
             return IND - o.IND;
-        return 
-
+        return AIRID - o.AIRID
     }
 
 }
