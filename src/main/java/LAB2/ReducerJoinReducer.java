1d0cae41ee09230dd75208fcd28e1682b3b375cd
niki <nikitapisarenko1995@gmail.com>
2021-12-24 21:47:18 +0300

    Scripted auto-commit on change (2021-12-24 21:47:18) by gitwatch.sh

--- src/main/java/LAB2/ReducerJoinReducer.java
+++ src/main/java/LAB2/ReducerJoinReducer.java
@@ -10,5 +10,5 @@ import java.io.IOException;
 public class ReducerJoinReducer extends  Reducer<AirportWritable,Text, IntWritable, Text> {
 
     public void reduce(AirportWritable key, Iterable<Text> values, Context context)
-        throws IOException
+        throws IOException, InterruptedException
 }
