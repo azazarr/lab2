c48c6f25825626bef163478421a4ec6ccde52b9f
niki <nikitapisarenko1995@gmail.com>
2021-12-24 19:43:23 +0300

    Scripted auto-commit on change (2021-12-24 19:43:23) by gitwatch.sh

--- src/main/java/LAB2/AirportMapper.java
+++ src/main/java/LAB2/AirportMapper.java
@@ -1,11 +1,11 @@
 package LAB2;
 
-
+import org.apache.hadoop.
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
     private static final int helper = 0;
     private static final int AIR_ID = 0;
-    protected void map (LongW)
+    protected void map (LongWritable key)
 
 
 }
